281b03caf9aa0a4b30cad864502607142ea242da
Dav-11 <48259205+Dav-11@users.noreply.github.com>
2024-12-07 13:07:44 +0100

    update

--- linux/conf-deb.sh
+++ linux/conf-deb.sh
@@ -18,8 +18,8 @@ sudo apt update && sudo apt -y upgrade
 #yes |sudo apt install gconf2
 
 
-# uninstall built in apps
-printf "[%s]\n ###############################\n\n Removing Bloatware... \n\n ###############################\n" "$(date +'%D%_H:%M')"
+# uninstall built in apps (gnome)
+printf "[%s]\n ###############################\n\n Removing Gnome Bloatware... \n\n ###############################\n" "$(date +'%D%_H:%M')"
 
 printf "[%s] Uninstalling installer... \n" "$(date +'%D%_H:%M')"
 yes |sudo apt remove ubiquity
@@ -58,8 +58,10 @@ sudo snap install vlc #vlc
 printf "[%s] Installing Curl... \n" "$(date +'%D%_H:%M')"
 sudo apt -y install curl
 
-printf "[%s] Installing Neofetch... \n" "$(date +'%D%_H:%M')"
-sudo apt -y install neofetch
+printf "[%s] Installing FastFetch... \n" "$(date +'%D%_H:%M')"
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+sudo apt update
+sudo apt -y install fastfetch
 
 printf "[%s] Installing Git... \n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
 sudo apt -y install git
@@ -68,13 +70,66 @@ printf "[%s] Installing FatPak... \n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
 sudo apt -y install flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-printf "[%s] Installing Zoom... \n" "$(date +'%D%_H:%M')"
-wget https://zoom.us/client/latest/zoom_amd64.deb
-sudo apt install ./zoom_amd64.deb
-sudo rm -f zoom_amd64.deb
+printf "[%s] Installing Helix editor... \n" "$(date +'%D%_H:%M')"
+sudo snap install helix --classic
 
-printf "[%s] Installing Telegram... \n" "$(date +'%D%_H:%M')"
-sudo snap install telegram-desktop
+printf "[%s] Installing Zellij... \n" "$(date +'%D%_H:%M')"
+sudo snap install zellij --classic
+
+printf "[%s] Installing Rustup... \n" "$(date +'%D%_H:%M')"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+printf "[%s] Build Alacritty... \n" "$(date +'%D%_H:%M')"
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+sudo apt install -y cmake \
+	g++ \
+	pkg-config \
+	libfreetype6-dev \
+	libfontconfig1-dev \
+	libxcb-xfixes0-dev \
+	libxkbcommon-dev \
+	python3
+
+# build
+cargo build --release
+
+# desktop entry
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
+# man
+sudo mkdir -p /usr/local/share/man/man1
+sudo mkdir -p /usr/local/share/man/man5
+scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+
+# https://github.com/alacritty/alacritty/blob/master/INSTALL.md
+
+# copy config_file
+mkdir -p "$HOME"/.config/alacritty
+curl -fsSL https://raw.githubusercontent.com/Dav-11/fast-start-config/main/config_files/alacritty.toml -o "$HOME"/.config/alacritty/alacritty.toml
+
+
+printf "[%s]\n ###############################\n\n Installing BASE_PROGRAMMING Apps... \n\n ###############################\n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
+
+printf "[%s] Installing Go... \n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
+sudo apt -y install golang-go
+
+printf "[%s] Installing VSCode... \n" "$(date +'%D%_H:%M')"
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt -y install apt-transport-https
+sudo apt -y update
+sudo apt -y install code
+
 
 if [ "$NETWORK" -gt "0" ]
 then
@@ -83,30 +138,6 @@ then
 	printf "[%s] Installing Easyssh... \n" "$(date +'%D%_H:%M')"
 	flatpak install flathub com.github.muriloventuroso.easyssh
 
-fi
-
-if [ "$BASE_PROGRAMMING" -gt "0" ]
-then
-	printf "[%s]\n ###############################\n\n Installing BASE_PROGRAMMING Apps... \n\n ###############################\n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
-
-	printf "[%s] Installing JDK... \n" "$(date +'%D%_H:%M')"
-	sudo apt -y install default-jdk
-
-	printf "[%s] Installing GO... \n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
-	sudo apt -y install golang-go
-
-	printf "[%s] Installing Rust... \n" "$(date +'%D%_H:%M')" | tee -a $LOG_FILE
-	sudo curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
-
-	printf "[%s] Installing VSCode... \n" "$(date +'%D%_H:%M')"
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-	sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-	rm -f packages.microsoft.gpg
-
-	sudo apt -y install apt-transport-https
-	sudo apt -y update
-	sudo apt -y install code
 fi
 
 if [ "$CHROME" -gt "0" ]
@@ -125,14 +156,6 @@ then
 	curl -fsSL https://get.docker.com -o get-docker.sh
  	sudo sh get-docker.sh
 	rm -f get-docker.sh
-fi
-
-if [ "$K8" -gt "0" ]
-then
-	printf "[%s]\n ###############################\n\n Installing KUBERNETES Apps... \n\n ###############################\n" "$(date +'%D%_H:%M')"
-	
-	printf "[%s] Installing Lens... \n" "$(date +'%D%_H:%M')"
-	sudo snap install kontena-lens --classic
 fi
 
 if [ "$WALLPAPER" -gt "0" ]
--- linux/linux.md
+++ linux/linux.md
@@ -2,21 +2,11 @@
 script for linux initial configuration
 
 ## Tested platforms
-- Fedora 36
 - Ubuntu 20.04
 
 # Usage
-
-## Fedora
-```bash
-curl -s https://raw.githubusercontent.com/Dav-11/fast-start-config/main/linux/conf-rhel.sh | bash
-```
 
 ## Debian-based
 ```bash
 curl -s https://raw.githubusercontent.com/Dav-11/fast-start-config/main/linux/conf-deb.sh | bash
 ```
-
-# TODO
-- [ ] Single starting script
-- [ ] Testing
